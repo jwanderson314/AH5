7a68496900d5e5766fa3bd279de02ae1f5e2fd10
Anderson <jason.anderson@fiserv.com>
2023-10-31 14:07:43 -0400

    Final Changes

--- Solution.java
+++ Solution.java
@@ -1,12 +1,27 @@
 class Solution {
     public static char mostFrequentChar(String s) {
-      
-      return;
+        int count = 0, max = 0;
+        char c = 0, fc = 0;
+        for(int i = 0; i < s.length(); i++){
+          c = s.charAt(i);
+          count = 0;
+          for(int o = 0; o < s.length(); o++){
+              if(count >= max){
+                  max = count;
+                  fc = c;
+              }
+              if(c == s.charAt(o))
+                  count++;
+          }
+        }
+        
+        return fc;
     }
   
     public static void main(String[] args) {
       // Try out your test cases:
       System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+
     }
 
   }
